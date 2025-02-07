cd1d6550754d97a0511796c7ceeea947b786f981
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-07 16:21:31 -0500

    feat: add header parser

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const lang = req.headers['accept-language'];
+    const userAgent = req.headers['user-agent'];
+    res.json({ipaddress: ip, language: lang, software: userAgent});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
